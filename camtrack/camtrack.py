8f0758766b02b8319aa44f4283b302a000b2146f
iisuslik43 <al.s.fedotov1998@yandex.ru>
2020-03-04 23:37:51 +0300

    success

--- camtrack/camtrack.py
+++ camtrack/camtrack.py
@@ -27,6 +27,11 @@ from _camtrack import (
     rodrigues_and_translation_to_view_mat3x4)
 
 
+INLIERS_MIN_SIZE = 0
+TRIANG_PARAMS = TriangulationParameters(4, 1e-2, 1e-2)
+DELTA = 7
+MIN_SIZE = 20
+
 def get_ransac(point_cloud_builder, corners_i, intrinsic_mat):
     intersection, (indexes_cloud, indexes_corners) = snp.intersect(point_cloud_builder.ids.flatten(),
                                                                    corners_i.ids.flatten(),
@@ -38,8 +43,9 @@ def get_ransac(point_cloud_builder, corners_i, intrinsic_mat):
                                                            corners_i.points[indexes_corners],
                                                            intrinsic_mat,
                                                            distCoeffs=None,
-                                                           reprojectionError=4)
+                                                           reprojectionError=1.5)
     except Exception:
+        print('Exception happened in solving PnP, continuing')
         return False, None, None, None
     rodrig = None
     cloud_points = None
@@ -48,10 +54,6 @@ def get_ransac(point_cloud_builder, corners_i, intrinsic_mat):
         cloud_points = point_cloud_builder.points[indexes_cloud][inliers.flatten()]
     return res_code, rodrig, inliers, cloud_points
 
-INLIERS_MIN_SIZE = 0
-TRIANG_PARAMS = TriangulationParameters(60, 1e-1, 1e-2)
-DELTA = 7
-MIN_SIZE = 20
 
 def update(i, corner_storage, view_mats, point_cloud_builder, intrinsic_mat, err_indexes, template, tqdm_iter):
     res_code, rodrig, inliers, cloud_points = get_ransac(point_cloud_builder, corner_storage[i], intrinsic_mat)
@@ -61,6 +63,7 @@ def update(i, corner_storage, view_mats, point_cloud_builder, intrinsic_mat, err
         view_mats[i] = rodrig
         err_indexes.remove(i)
         tqdm_iter.update()
+
     def update_cloud(j):
         if i != j and j not in err_indexes:
             correspondences_i = build_correspondences(corner_storage[i], corner_storage[j])
@@ -69,6 +72,7 @@ def update(i, corner_storage, view_mats, point_cloud_builder, intrinsic_mat, err
                                                                              view_mats[j],
                                                                              intrinsic_mat,
                                                                              TRIANG_PARAMS)
+
             if len(points3d_j) > MIN_SIZE:
                 point_cloud_builder.add_points(corr_ids_j.astype(np.int64), points3d_j)
                 tqdm_iter.set_description(template.format(len(inliers),
@@ -76,12 +80,8 @@ def update(i, corner_storage, view_mats, point_cloud_builder, intrinsic_mat, err
                                                           len(point_cloud_builder._points)))
 
     if i not in err_indexes:
-        # for j in range(i + 1, min(i + DELTA, len(view_mats) - 1)):
-        #     update_cloud(j)
         for j in range(i - 1, max(0, i - DELTA) - 1, -1):
             update_cloud(j)
-        # for j in range(max(0, i - DELTA), min(i + DELTA, len(view_mats) - 1)):
-        #     update_cloud(j)
 
 def track_and_calc_colors(camera_parameters: CameraParameters,
                           corner_storage: CornerStorage,
@@ -99,12 +99,14 @@ def track_and_calc_colors(camera_parameters: CameraParameters,
     i_1 = known_view_1[0]
     i_2 = known_view_2[0]
     print('Known frames are', i_1, 'and', i_2)
+    global INLIERS_MIN_SIZE, DELTA, MIN_SIZE, TRIANG_PARAMS
     view_mat3x4_1 = pose_to_view_mat3x4(known_view_1[1])
     view_mat3x4_2 = pose_to_view_mat3x4(known_view_2[1])
     correspondences = build_correspondences(corner_storage[i_1], corner_storage[i_2])
-
+    print(len(correspondences.ids))
     points3d, corr_ids, median_cos = triangulate_correspondences(correspondences,
-                                                                 view_mat3x4_1, view_mat3x4_2,
+                                                                 view_mat3x4_1,
+                                                                 view_mat3x4_2,
                                                                  intrinsic_mat,
                                                                  TRIANG_PARAMS)
     view_mats, point_cloud_builder = [view_mat3x4_1 for _ in corner_storage], PointCloudBuilder(
@@ -119,17 +121,22 @@ def track_and_calc_colors(camera_parameters: CameraParameters,
     res_code, rodrig, inliers_2, cloud_points = get_ransac(point_cloud_builder, corner_storage[i_2],
                                                            intrinsic_mat)
     view_mats[i_2] = rodrig
-    INLIERS_MIN_SIZE = min(len(inliers_1), len(inliers_2)) - 20
+    INLIERS_MIN_SIZE = min(len(inliers_1), len(inliers_2)) - 20 * max(len(corner_storage) // 100, 1)
     descr_template = 'Point cloud calc - {} inliers, {} points found, cloud size is {}'
 
     tqdm_iterator = tqdm(range(len(corner_storage) - 2), total=len(corner_storage),
                          desc=descr_template.format('?', '?', '?'))
 
     params = [corner_storage, view_mats, point_cloud_builder, intrinsic_mat, err_indexes, descr_template, tqdm_iterator]
+    DELTA = 5 * max(len(corner_storage) // 100, 1)
+    MIN_SIZE = 20 * max(len(corner_storage) // 100, 1)
+    TRIANG_PARAMS = TriangulationParameters(2, 1e-2, 1e-2)
     for i in range(i_1 + 1, i_2):
         update(i, *params)
-    DELTA = 20
-    MIN_SIZE = 20
+    print('Points between handled')
+    DELTA = 20 * max(len(corner_storage) // 100, 1)
+    MIN_SIZE = 20 * max(len(corner_storage) // 100, 1)
+    TRIANG_PARAMS = TriangulationParameters(4, 1e-2, 1e-2)
     for i in range(i_1, -1, -1):
         if i in err_indexes:
             update(i, *params)
@@ -137,9 +144,13 @@ def track_and_calc_colors(camera_parameters: CameraParameters,
         if i in err_indexes:
             update(i, *params)
 
-    for i in range(len(corner_storage)):
+    for i in range(i_1, -1, -1):
         if i in err_indexes:
-            view_mats[i] = view_mats[i - 1] if i != 0 else view_mats[i + 1]
+            view_mats[i] = view_mats[i + 1]
+
+    for i in range(i_2, len(corner_storage)):
+        if i in err_indexes:
+            view_mats[i] = view_mats[i - 1]
     calc_point_cloud_colors(
         point_cloud_builder,
         rgb_sequence,
